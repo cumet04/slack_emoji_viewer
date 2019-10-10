8ca4110af13ebca8a76ed74f9b30c3d0c209ffa5
cumet04 <medalhkr@gmail.com>
2019-10-10 22:59:31 +0900

    add mock for dev

--- middleware/emoji.js
+++ middleware/emoji.js
@@ -1,13 +1,21 @@
 const axios = require('axios')
 
 export default function({ store }) {
+  // const source = slack_get()
+  const source = mock_get()
+  return source.then(res => {
+    store.commit('emoji/set', res.data.emoji)
+  })
+}
+
+function slack_get() {
   const max_count = 2000
-  return axios
-    .post(
-      'https://oro.slack.com/api/emoji.adminList',
-      [`token=${process.env.SLACK_TOKEN}`, `count=${max_count}`].join('&')
-    )
-    .then(res => {
-      store.commit('emoji/set', res.data.emoji)
-    })
+  return axios.post(
+    'https://oro.slack.com/api/emoji.adminList',
+    [`token=${process.env.SLACK_TOKEN}`, `count=${max_count}`].join('&')
+  )
+}
+
+function mock_get() {
+  return axios.get('http://localhost:3000/_dummy_emojis')
 }
