101ebf872c769dc1f02500ba39dd3e7c5513f51f
cumet04 <medalhkr@gmail.com>
2020-02-29 18:53:32 +0000

    impl index

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,43 @@
 <template>
   <main>
-    aaa
+    <input type="text" v-model="keyword" placeholder="Search" />
+    <ul class="list">
+      <li
+        v-for="emoji in all"
+        :key="emoji.name"
+        v-show="emoji.name.includes(keyword)"
+        class="emoji"
+      >
+        <emoji :emoji="emoji" />
+      </li>
+    </ul>
   </main>
 </template>
 
-<script></script>
+<script>
+import Emoji from "~/components/Emoji.vue";
 
-<style scoped lang="scss"></style>
+export default {
+  components: {
+    emoji: Emoji
+  },
+  data() {
+    return {
+      all: this.$store.getters["emoji/all"],
+      keyword: ""
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.list {
+  display: flex;
+  flex-wrap: wrap;
+  width: 800px;
+}
+
+.emoji {
+  margin: 2px;
+}
+</style>
