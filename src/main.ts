241516542b015508b33a8154d357c2a0662de70c
cumet04 <medalhkr@gmail.com>
2020-06-06 07:14:29 +0900

    move layout

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import { createStore } from "./store";
 import { router } from "./router";
-import App from "./layouts/default.vue";
+import App from "./App.vue";
 import "./assets/css/base.css";
 
 createApp(App).use(createStore()).use(router).mount("#app");
