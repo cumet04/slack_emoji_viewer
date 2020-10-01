fd03c9f5fc43c5714048e009237a7a41e3d82016
medalhkr <medalhkr@gmail.com>
2020-10-01 23:37:12 +0900

    remove unused now

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,10 @@
 import { createEmojiStore } from "./stores/emoji";
-import { createStockStore } from "./stores/stock";
 import { createWorkspaceStore } from "./stores/workspace";
 import { createPreferenceStore } from "./stores/preference";
 import { App, InjectionKey, inject } from "vue";
 
 export type Store = {
   emoji: ReturnType<typeof createEmojiStore>;
-  stock: ReturnType<typeof createStockStore>;
   workspace: ReturnType<typeof createWorkspaceStore>;
   preference: ReturnType<typeof createPreferenceStore>;
   install(app: App): void;
@@ -16,7 +14,6 @@ const storeKey = Symbol() as InjectionKey<Store>;
 export function createStore(): Store {
   const store = {
     emoji: createEmojiStore(),
-    stock: createStockStore(),
     workspace: createWorkspaceStore(),
     preference: createPreferenceStore(),
     install(app: App) {
