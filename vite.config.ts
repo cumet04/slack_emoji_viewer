da7e5d70acc1374ccb5c7410d79ee1be43d3ee04
cumet04 <medalhkr@gmail.com>
2021-10-09 14:19:59 +0900

    bump all
    without
    * eslint
    * tsc

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,22 @@
+import { defineConfig } from "vite";
+import vue from '@vitejs/plugin-vue'
 import replace from "@rollup/plugin-replace";
-import { UserConfig } from "vite";
 
 const url = process.env["GITHUB_SERVER_URL"]
   ? `${process.env["GITHUB_SERVER_URL"]}/${process.env["GITHUB_REPOSITORY"]}`
   : "https://example.com";
 
-const config: UserConfig = {
-  base: "/slack_emoji_viewer",
-  rollupInputOptions: {
-    plugins: [
-      replace({
+export default defineConfig({
+  base: "/slack_emoji_viewer/",
+  plugins: [
+    vue(),
+    replace({
+      preventAssignment: true,
+      values: {
         __repository_url__: url,
         __github_sha__: process.env["GITHUB_SHA"] || "xxxxxxx",
         __build_time__: Date.now().toString(),
-      }),
-    ],
-  },
-};
-
-export default config;
+      }
+    }),
+  ],
+});
