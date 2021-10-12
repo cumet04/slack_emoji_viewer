63228be98aa584ae9f3356644ed93b9a63087310
cumet04 <medalhkr@gmail.com>
2021-10-12 22:31:48 +0900

    tmp

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"embed"
+	"fmt"
 	"io/fs"
 	"net/http"
+	"net/url"
+	"strconv"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -36,5 +40,40 @@ func main() {
 		}
 	})
 
+	router.POST("/emojis/", handleEmojiIndex)
+
 	router.Run("localhost:8080")
+}
+
+func handleEmojiIndex(ctx *gin.Context) {
+	type Request struct {
+		Domain string `json:"domain"`
+		Count  int    `json:"count"`
+		Token  string `json:"token"`
+	}
+	var r Request
+	if err := ctx.ShouldBindJSON(&r); err != nil {
+		ctx.String(400, "failed to parse request body")
+		return
+	}
+
+	result, err := fetchEmojiAll(r.Domain, r.Count, r.Token)
+	if err != nil {
+		panic(err)
+	}
+
+	ctx.Header("Content-Type", "application/json; charset=utf-8")
+	ctx.String(200, result)
+}
+
+func fetchEmojiAll(domain string, count int, token string) (string, error) {
+	u := fmt.Sprintf("https://%s.slack.com/api/emoji.adminList", domain)
+	resp, err := http.PostForm(u, url.Values{"token": {token}, "count": {strconv.Itoa(count)}})
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	return buf.String(), nil
 }
